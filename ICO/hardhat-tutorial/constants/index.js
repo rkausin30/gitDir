cb5a567c62ce7a4207a29f698e8bb365e05e9aa1
rkausin30 <ryanjkausin@gmail.com>
2022-12-27 20:14:33 -0800

    Added Token ICO project

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xa7282799D9Aa1E7e8D9Ec46eE395225A846401ee";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
