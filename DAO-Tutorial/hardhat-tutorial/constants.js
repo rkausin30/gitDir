8bf3ed519c759efa267dea36c9934ccd74c77536
rkausin30 <ryanjkausin@gmail.com>
2022-12-28 16:57:17 -0800

    Committing DAO Tutorial

---
+++ DAO-Tutorial/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xa7282799D9Aa1E7e8D9Ec46eE395225A846401ee";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
